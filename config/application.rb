c76620a55868086f3dc5aec01cffff7f275e7e57
Brandon Jernigan <jerniganbrandon@gmail.com>
2014-09-23 19:21:52 -0600

    Fixed flash messages!

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,6 @@ Bundler.require(*Rails.groups)
 
 module Twonoo
   class Application < Rails::Application
-
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
