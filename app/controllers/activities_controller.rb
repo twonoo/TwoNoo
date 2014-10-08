3dd32d5cfb1050af912572d89e0f0f477e8ef6d8
Brandon Jernigan <jerniganbrandon@gmail.com>
2014-10-08 00:35:21 -0600

    Fix render statement

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -41,12 +41,9 @@ class ActivitiesController < ApplicationController
     @activities = Activity.terms(params[:terms])
     @activities = @activities.joins(:activity_types).where('activity_types.id' => params[:type]) unless params[:type].blank?
     @activities = @activities.where('datetime BETWEEN ? AND ?', from_date, end_date)
-    @activities = @activities.within(params[:distance], origin: search_coordinates)
-
-    if @actvities.blank?
-      render 'noresults'
-    end
     @activities = @activities.within(params[:distance], origin: search_coordinates).order('datetime ASC')
+    
+    render 'noresults' unless @activities.within(params[:distance], origin: search_coordinates).order('datetime ASC').present? 
   end
 
   def user
