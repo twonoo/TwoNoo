eff57fc75f399a63613cf63a8dd3bb6ca525ff1a
Brandon Jernigan <jerniganbrandon@gmail.com>
2014-10-11 23:13:12 -0600

    Search Fix

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,7 +19,7 @@ class Profile < ActiveRecord::Base
 	def self.terms(terms)
 		query = []
 			terms.split.each do |t|
-				query << "(first_name LIKE '%#{t}%' OR last_name LIKE '%#{t}%')"
+				query << "(first_name = '#{t}' OR last_name LIKE '#{t}')"
 			end
 			where(query.join(" AND "))
 	end
