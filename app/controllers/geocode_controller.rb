02ad60122fa6bc9da1259de4b63547fb524aeec9
Steven Betts <steve_betts345@hotmail.com>
2014-10-14 14:38:50 -0600

    Made the create event default to the user's current location. Changed Keefe's about profile picture

--- app/controllers/geocode_controller.rb
+++ app/controllers/geocode_controller.rb
@@ -6,4 +6,12 @@ class GeocodeController < ApplicationController
 		  format.json { render :json => data}
 		end
   end
+
+  def searchCityState
+  	location = Geocoder.search(params[:location]).first
+  	data = {city: location.city, state: location.state_code }
+		respond_to do |format|
+		  format.json { render :json => data}
+		end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'geocode/search'
+  get 'geocode/searchCityState'
   post 'search' => 'welcome#search', as: :search
   get 'alerts/index'
   get 'alerts' => 'alerts#index', as: :alerts
