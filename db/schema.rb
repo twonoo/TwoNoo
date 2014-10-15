d471cdece714aa80f8dc72c65f18a9d08daf6763
Steven Betts <steve_betts345@hotmail.com>
2014-10-14 22:17:02 -0600

    Got rid of the stash message on the drop down

--- db/schema.rb
+++ db/schema.rb
@@ -179,16 +179,6 @@ ActiveRecord::Schema.define(version: 20141014082446) do
     t.datetime "updated_at"
   end
 
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
-
   create_table "transaction_types", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -197,14 +187,14 @@ ActiveRecord::Schema.define(version: 20141014082446) do
 
   create_table "transactions", force: true do |t|
     t.integer  "transaction_type_id"
-    t.integer  "user_id"
-    t.integer  "activity_id"
     t.integer  "amount"
     t.float    "cost"
     t.integer  "balance"
-    t.text     "comment"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "comment"
+    t.integer  "activity_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
