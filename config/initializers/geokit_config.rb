916f7f89e7e925b521a44405510513b0e36c7e05
Brandon Jernigan <jerniganbrandon@gmail.com>
2014-10-21 19:11:54 -0600

    API Key for GeoKit

---
+++ config/initializers/geokit_config.rb
@@ -0,0 +1,3 @@
+# You can also use the free API key instead of signed requests
+# See https://developers.google.com/maps/documentation/geocoding/#api_key
+Geokit::Geocoders::GoogleGeocoder.api_key = 'AIzaSyCNhGdKD91B3SVN5kH1oE8y1hS-swp2e4k'
