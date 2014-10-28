d8868df682fc900adfe82719ca4258b8c4ae93b3
Steven Betts <steve_betts345@hotmail.com>
2014-10-28 14:52:06 -0600

    Fixed a problem with the autoscrolling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,14 +86,23 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.mandrillapp.com",
     port: 587,
     domain: "twonoo.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "info@twonoo.com",
-    password: "Tw0#oo01"
+    password: "FWDtyP_2VA85dpsXhuudoA"
   }
+#  config.action_mailer.smtp_settings = {
+#    address: "smtp.gmail.com",
+#    port: 587,
+#    domain: "twonoo.com",
+#    authentication: "plain",
+#    enable_starttls_auto: true,
+#    user_name: "info@twonoo.com",
+#    password: "Tw0#oo01"
+#  }
 
 
   config.secret_key_base = 'f875f67fdc2bc511c4130f11dcbfc8143bc2a74184b2ccdd66b0c2243ffc928504e3f4dcb929a809729107cf278e13184f96e4cd4b74646f793c5753b6033194'
