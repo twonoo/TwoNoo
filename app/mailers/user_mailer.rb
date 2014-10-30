1b7ff7d298b1492920042c4e0cba4ba54c142ab5
Steven Betts <steve_betts345@hotmail.com>
2014-10-30 16:53:02 -0600

    Mandrill Integration

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -40,36 +40,115 @@ ActionMailer::Base.default_url_options[:host] = "www.twonoo.com"
 
   def feedback_from_user(user, feedback)
     @user = user
-    @feedback = feedback
-    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#    @feedback = feedback
+#    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#
+#    mail(to: "feedback@twonoo.com",
+#      subject: "#{@user.name} has sent you feedback.",
+#      template_name: "feedback")
+    # Mandrill integration
 
-    mail(to: "feedback@twonoo.com",
-      subject: "#{@user.name} has sent you feedback.",
-      template_name: "feedback")
+    mandrill_to = [{:email => 'feedback@twonoo.com', :type => 'to'}]
+    m = Mandrill::API.new
+    t = 'feedback_from_user'
+    tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+    message = {  
+     :from_name=> user.name,  
+     :to=>mandrill_to,  
+     :from_email=>user.email,
+     :merge_language=>"mailchimp",
+     :global_merge_vars=>[
+      {"name"=>"USER_NAME", "content"=>user.name},
+      {"name"=>"FEEDBACK", "content"=>feedback}
+      ] 
+    }  
+
+    sending = m.messages.send_template t, tc, message  
+    logger.info sending
   end
 
   def feedback(feedback)
-    @feedback = feedback
-    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#    @feedback = feedback
+#    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#
+#    mail(to: "feedback@twonoo.com", subject: "Somebody has sent you feedback.")
+    # Mandrill integration
 
-    mail(to: "feedback@twonoo.com", subject: "Somebody has sent you feedback.")
+    mandrill_to = [{:email => 'feedback@twonoo.com', :type => 'to'}]
+    m = Mandrill::API.new
+    t = 'feedback'
+    tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+    message = {  
+     :to=>mandrill_to,  
+     :merge_language=>"mailchimp",
+     :global_merge_vars=>[
+      {"name"=>"FEEDBACK", "content"=>feedback}
+      ] 
+    }  
+
+    sending = m.messages.send_template t, tc, message  
+    logger.info sending
   end
 
   def activity_invite(user, activity, emails)
     @user = user
-    @activity = activity
-    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#    @activity = activity
+#    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#
+#    mail(bcc: emails, subject: "#{@user.name} has invited you to #{@activity.activity_name}!")
+    # Mandrill integration
 
-    mail(bcc: emails, subject: "#{@user.name} has invited you to #{@activity.activity_name}!")
+    @emails = emails.split(',')
+    mandrill_to = @emails.map{|email| {:email => email, :type => 'bcc'}}
+    m = Mandrill::API.new
+    t = 'activity_invite'
+    tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+    message = {  
+     :from_name=> user.name,  
+     :to=>mandrill_to,  
+     :from_email=>"no-reply@twonoo.com",
+     :merge_language=>"mailchimp",
+     :global_merge_vars=>[
+      {"name"=>"INVITER", "content"=>user.name},
+      {"name"=>"ACTIVITY_ID", "content"=>user.name},
+      {"name"=>"ACTIVITY_NAME", "content"=>user.name},
+      {"name"=>"ACTIVITY_DESC", "content"=>activity.description},
+      {"name"=>"REFERRER", "content"=>@user.id.to_s}
+      ] 
+    }  
+
+    sending = m.messages.send_template t, tc, message  
+    logger.info sending
+
   end
 
   def new_follower(user, follower)
     @user = user
 
     if (@user.profile.notification_setting.new_follower)
-      @follower = follower
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "#{@follower.name} is now following you.")
+#      @follower = follower
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "#{@follower.name} is now following you.")
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'new_follower'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> follower.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"FOLLOWER_ID", "content"=>follower.id},
+        {"name"=>"FOLLOWER_NAME", "content"=>follower.name}
+        ] 
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
@@ -77,23 +156,64 @@ ActionMailer::Base.default_url_options[:host] = "www.twonoo.com"
     @user = user
 
     if (@user.profile.notification_setting.new_message)
-      @message = message
-      @sender = sender
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "You have a new message from #{@sender.name}")
+#      @message = message
+#      @sender = sender
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "You have a new message from #{@sender.name}")
+
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'new_message'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> sender.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"MESSAGE", "content"=>message},
+        {"name"=>"MESSAGER_NAME", "content"=>sender.name}
+        ] 
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
   def new_rsvp(user, attendee, activity)
-    logger.info "new_rsvp testering"
     @user = user
 
     if (@user.profile.notification_setting.new_rsvp)
-      logger.info "new_rsvp testering receiving"
-      @attendee = attendee
-      @activity = activity
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "#{@attendee.name} is joining #{@activity.activity_name}")
+#      @attendee = attendee
+#      @activity = activity
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "#{@attendee.name} is joining #{@activity.activity_name}")
+      
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'new_rsvp'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> attendee.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"ATTENDEE", "content"=>attendee.name},
+        {"name"=>"ACTIVITY_ID", "content"=>activity.id},
+        {"name"=>"ACTIVITY_NAME", "content"=>activity.activity_name}
+        ] 
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
@@ -101,10 +221,33 @@ ActionMailer::Base.default_url_options[:host] = "www.twonoo.com"
     @user = user
 
     if (@user.profile.notification_setting.new_following_activity)
-      @organizer = organizer
-      @activity = activity
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "#{@organizer.name} just created #{@activity.activity_name} on TwoNoo!")
+#      @organizer = organizer
+#      @activity = activity
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "#{@organizer.name} just created #{@activity.activity_name} on TwoNoo!")
+
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'new_following_activity'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> organizer.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"ORGANIZER", "content"=>organizer.name},
+        {"name"=>"ORGANIZER_ID", "content"=>organizer.id},
+        {"name"=>"ACTIVITY_ID", "content"=>activity.id},
+        {"name"=>"ACTIVITY_NAME", "content"=>activity.activity_name}
+        ] 
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
@@ -112,9 +255,30 @@ ActionMailer::Base.default_url_options[:host] = "www.twonoo.com"
     @user = user
 
     if (@user.profile.notification_setting.attending_activity_update)
-      @activity = activity
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "#{@activity.activity_name} has been updated on TwoNoo!")
+#      @activity = activity
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "#{@activity.activity_name} has been updated on TwoNoo!")
+
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'attending_activity_update'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> organizer.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"ACTIVITY_ID", "content"=>activity.id},
+        {"name"=>"ACTIVITY_NAME", "content"=>activity.activity_name}
+        ] 
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
@@ -122,11 +286,34 @@ ActionMailer::Base.default_url_options[:host] = "www.twonoo.com"
     @user = user
 
     if (@user.profile.notification_setting.comment_on_owned_activity)
-      @activity = activity
-      @commenter = commenter
-      @comment = comment
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "#{@commenter.name} posted a new comment on #{@activity.activity_name}")
+#      @activity = activity
+#      @commenter = commenter
+#      @comment = comment
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "#{@commenter.name} posted a new comment on #{@activity.activity_name}")
+
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'comment_on_owned_activity'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> commenter.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"COMMENTER_NAME", "content"=>commenter.name},
+        {"name"=>"COMMENTER_ID", "content"=>commenter.id},
+        {"name"=>"COMMENT", "content"=>comment},
+        {"name"=>"ACTIVITY_ID", "content"=>activity.id},
+        {"name"=>"ACTIVITY_NAME", "content"=>activity.activity_name}
+        ] 
+      }  
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
@@ -134,23 +321,69 @@ ActionMailer::Base.default_url_options[:host] = "www.twonoo.com"
     @user = user
 
     if (@user.profile.notification_setting.comment_on_attending_activity)
-      @activity = activity
-      @commenter = commenter
-      @comment = comment
-      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-      mail(to: @user.email, subject: "#{@commenter.name} posted a new comment on #{@activity.activity_name}")
+#      @activity = activity
+#      @commenter = commenter
+#      @comment = comment
+#      attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#      mail(to: @user.email, subject: "#{@commenter.name} posted a new comment on #{@activity.activity_name}")
+
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'comment_on_attending_activity'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :from_name=> commenter.name,  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"COMMENTER_NAME", "content"=>commenter.name},
+        {"name"=>"COMMENTER_ID", "content"=>commenter.id},
+        {"name"=>"COMMENT", "content"=>comment},
+        {"name"=>"ACTIVITY_ID", "content"=>activity.id},
+        {"name"=>"ACTIVITY_NAME", "content"=>activity.activity_name}
+        ] 
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
     end
   end
 
   def alert(user, activity, alert)
     @user = user
 
-    @activity = activity
-    @alert = alert
-    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
-    puts "mailing"
-    mail(to: @user.email, subject: "#{@activity.activity_name} matched one of your alerts!")
-    puts "mailed"
+#    @activity = activity
+#    @alert = alert
+#    attachments.inline['twonoo-logo.png'] = File.read("#{Rails.root}/app/assets/images/twonoo_logo_small.png")
+#    puts "mailing"
+#    mail(to: @user.email, subject: "#{@activity.activity_name} matched one of your alerts!")
+#    puts "mailed"
+
+      # Mandrill integration
+
+      mandrill_to = [{:email => user.email, :type => 'to'}]
+      m = Mandrill::API.new
+      t = 'alert'
+      tc = [{"name" => "inviter", "content" => "TwoNoo Testerino"}]
+      message = {  
+       :to=>mandrill_to,  
+       :merge_language=>"mailchimp",
+       :global_merge_vars=>[
+        {"name"=>"USER_NAME", "content"=>user.name},
+        {"name"=>"USER_EMAIL", "content"=>user.email},
+        {"name"=>"ALERT_KEYWORDS", "content"=>alert.keywords},
+        {"name"=>"ACTIVITY_ID", "content"=>activity.id},
+        {"name"=>"ACTIVITY_NAME", "content"=>activity.activity_name},
+        {"name"=>"ACTIVITY_DESC", "content"=>activity.description}
+        ]
+      }  
+
+      sending = m.messages.send_template t, tc, message  
+      logger.info sending
   end
 
 end
