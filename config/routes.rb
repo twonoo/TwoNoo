469708c2134c6aa9350142502b705be469898282
Steven Betts <steve_betts345@hotmail.com>
2014-11-03 15:40:34 -0700

    Added a tool tip to the search page turned the search into a GET instead of a POST

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get 'geocode/search'
   get 'geocode/searchCityState'
-  post 'search' => 'welcome#search', as: :search
+  #post 'search' => 'welcome#search', as: :search
+  get 'search' => 'welcome#search', as: :search
   get 'alerts/index'
   get 'alerts' => 'alerts#index', as: :alerts
   post 'alerts/create' => 'alerts#create', as: :alert_create
