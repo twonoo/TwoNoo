576cbdab4440efff6a3d9685ec73337ee6a2190b
Brandon Jernigan <jerniganbrandon@gmail.com>
2014-11-04 10:28:30 -0700

    Ugh fix search

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'geocode/search'
   get 'geocode/searchCityState'
   post 'search' => 'welcome#search', as: :search
-  get 'search' => 'welcome#search', as: :search
+  #get 'search' => 'welcome#search', as: :search
   get 'alerts/index'
   get 'alerts' => 'alerts#index', as: :alerts
   post 'alerts/create' => 'alerts#create', as: :alert_create
