6abc3d09a4f8be289d3480fbae690376bd35bdb9
Daniel Logan <dlogan21@gmail.com>
2015-02-11 16:17:43 -0700

    gssý5:q!kbkbkbkb:q!

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -237,7 +237,7 @@ Devise.setup do |config|
       '2MpNyC_RjDUdADBDQs752GLz', {
         access_type: 'offline',
         scope: 'https://www.googleapis.com/auth/calendar',
-        redirect_uri:'http://www.twonoo.com/auth/google_oauth2/callback'
+        redirect_uri:'https://www.twonoo.com/auth/google_oauth2/callback'
       }
 
   # ==> Warden configuration
