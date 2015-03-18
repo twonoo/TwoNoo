b4a87f476331a5d0863f70a5ebf90d585f7ec355
Daniel Logan <dlogan21@gmail.com>
2015-03-17 18:25:04 -0600

    background job

--- app/controllers/background_jobs_controller.rb
+++ app/controllers/background_jobs_controller.rb
@@ -1,15 +1,12 @@
 class BackgroundJobsController < ApplicationController
 
   def create
-    user = User.unscoped.where(id: current_user.id).first
-    user.find_people
-    render json: {}, status: 200
-    #job = FindPeopleJob.perform_later(current_user.id)
-    #if job && job.job_id
-      #render json: {}, status: 200
-    #else
-     # render json: {}, status: 500
-    #end
+    job = FindPeopleJob.perform_later(current_user.id)
+    if job && job.job_id
+      render json: {}, status: 200
+    else
+      render json: {}, status: 500
+    end
   end
 
 end
