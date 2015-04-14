9d9b8cac5faa80207d48989f33577decdc7be58d
Daniel Logan <dlogan21@gmail.com>
2015-04-14 16:29:37 -0600

    another commit - still broken

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -3,7 +3,7 @@ module LikesHelper
   def like_button(activity_id)
     "<div class='like-widget' activity-id='#{activity_id}'>
     <a href='#' class='like-button'></a>
-                <span class='like-counter'></span>
+                <span class='like-counter'>0</span>
     </div>".html_safe
   end
 
