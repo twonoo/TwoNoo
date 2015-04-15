c05163b10dc78b9bda830137cf93808791b6f616
Peter Mills <waveminded@gmail.com>
2015-04-15 14:55:33 -0600

    styles for like button

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,10 +1,10 @@
 module LikesHelper
 
   def like_button(activity_id)
-    "<div class='like-widget' activity-id='#{activity_id}'>
-    <a href='#' class='like-button'></a>
-                <span class='like-counter'>0</span>
-    </div>".html_safe
-  end
+   "<div class='like-widget' activity-id='#{activity_id}'>
+   <span class='like-button'><span class='like-button-text'>Like</span></span>
+               <span class='like-counter'>0</span>
+   </div>".html_safe
+ end
 
 end
