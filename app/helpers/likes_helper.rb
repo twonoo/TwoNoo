37d10a5dbc8480d8cedf951918ac0fdb8cc0c522
Daniel Logan <dlogan21@gmail.com>
2015-04-16 14:07:37 -0600

    color change on background of likes

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,5 +1,8 @@
 module LikesHelper
 
+  #It is important that non of the classes change on this.
+  #Javascript uses it to make things work.
+  #If you do need to change something, please also adjust likes.js.erb
   def like_button(activity_id)
    "<div class='like-widget' activity-id='#{activity_id}'>
    <span class='like-button'><span class='like-button-text'>Like</span></span>
