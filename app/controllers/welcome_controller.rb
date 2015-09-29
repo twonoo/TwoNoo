0796c87351f9065606f1860f07f9e45bc06d5f18
Dylan Cashman <dylan.cashman@annkissam.com>
2015-09-29 09:15:05 -0400

    Removing debugging line

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -46,7 +46,7 @@ class WelcomeController < ApplicationController
     lat,lon,outside_supported_area = determine_lat_lon
     search_coordinates = [lat.to_f,lon.to_f]
     @showCreateAlert = false
-    
+
     params[:distance] ||= 25
     params[:terms] ||= ''
 
@@ -93,7 +93,7 @@ class WelcomeController < ApplicationController
 
     @page_increment = 9
     @users_offset = [params[:users_offset].to_i, 0].max
-    @users_max = [params[:users_max].to_i, (1)].max
+    @users_max = [params[:users_max].to_i, (@page_increment + 10)].max
     @activities_offset = [params[:activities_offset].to_i, 0].max
     @activities_max = [params[:activities_max].to_i, @page_increment].max
     @total_users = @users.present? ? @users.count : 0
